11f5912457a2f3e4231ff93cbbbf8fd6a1a58cab
qqqqqcy <qqqqqcy@gmail.com>
2020-06-19 14:00:05 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          QQQQQCY's BLOG
         </a>
       </header>
     </div>
